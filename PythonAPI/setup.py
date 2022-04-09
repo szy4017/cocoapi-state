c9d66b544baf37195a7f84df69a79f5b01c01e63
施振宇 <2889035022@qq.com>
2022-04-09 17:35:10 +0800

    update setup.py

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -9,19 +9,19 @@ ext_modules = [
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
         include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=['-std=c99'],
     )
 ]
 
 setup(
-    name='pycocotools',
-    packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
+    name='pycocotools-state',
+    packages=['pycocotools-state'],
+    package_dir = {'pycocotools-state': 'pycocotools-state'},
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
         'matplotlib>=2.1.0'
     ],
-    version='2.0',
+    version='1.0',
     ext_modules= ext_modules
 )
